04a31229c9f5186a224be86b6773dbeda5afe853
Sreehari Mullapulli <sreehari.sm@gmail.com>
2019-04-25 00:43:34 +0200

    Login backend

--- server-certify/src/controllers/AuthenticationController.js
+++ server-certify/src/controllers/AuthenticationController.js
@@ -10,6 +10,40 @@ module.exports = {
              error: 'This email account already in use.'
          })
      }
+        },
+
+async   login (req, res) {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        
+        if(!user) {
+            return res.status(403).send({
+                error: 'The login information was incorrect.'
+            })
         }
-    }
+
+        const isPasswordValid = password === user.password
+        if(!isPasswordValid){
+            return res.status(403).send({
+                error: 'The login information was incorrect.'
+            })
+        }
+
+        const userJson = user.toJSON()
+        res.send({
+            user: userJson
+        })
+        
+     } catch (err) {
+        res.status(500).send({
+        error: 'An error has occured while trying to login.'
+         })
+     }
+        }        
+}
  
