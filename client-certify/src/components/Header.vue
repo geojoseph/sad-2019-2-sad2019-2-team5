60897ffd3acc87c0c328c3c2687e400054f960ae
Sreehari Mullapulli <sreehari.sm@gmail.com>
2019-04-28 08:53:38 +0200

    Logout

--- client-certify/src/components/Header.vue
+++ client-certify/src/components/Header.vue
@@ -37,6 +37,14 @@
       @click= "navigateTo({name: 'register'})"
       >
       Sign Up
+      </v-btn>
+     <v-btn
+      v-if= "$store.state.isUserLoggedIn"
+      flat
+      dark
+      @click="logout"
+      >
+      Log Out
      </v-btn>
     </v-toolbar-items>
 </v-toolbar>
@@ -47,6 +55,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
