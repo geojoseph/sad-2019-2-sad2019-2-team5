06dd436c9d8b257ffdd30645d00432e65c3daafb
Sreehari Mullapulli <sreehari.sm@gmail.com>
2019-05-06 00:01:35 +0200

    update on add certificate page

--- client-certify/src/components/Addcertificate.vue
+++ client-certify/src/components/Addcertificate.vue
@@ -1,14 +1,79 @@
 <template >
-    <v-content v-if= "!!$store.state.isUserLoggedIn">
+    <v-content >
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md10>
               <panel title="Add Certificate Information">
-                <v-text-field
-                label="Certificate Name"
-                required
-                >
-                </v-text-field>
+  <v-stepper v-model="e6" vertical>
+    <v-stepper-step :complete="e6 > 1" step="1">
+      *Certificate Name
+      <v-text-field
+      label="Provide certificate name"
+      :rules="[required]"
+      v-model="certificate.name"
+      required>
+      </v-text-field>
+   </v-stepper-step>
+   <v-stepper-content step="1">
+      <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
+      <v-btn flat>Cancel</v-btn>
+   </v-stepper-content>
+
+    <v-stepper-step :complete="e6 > 2" step="2">
+      *Certificate provider
+      <v-text-field
+      label="Provide certificate issuer"
+      :rules="[required]"
+      v-model="certificate.provider"
+      required>
+      </v-text-field>
+      </v-stepper-step>
+   <v-stepper-content step="2">
+      <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
+      <v-btn flat @click="e6 = 1">Cancel</v-btn>
+   </v-stepper-content>
+
+   <v-stepper-step :complete="e6 > 3" step="3">
+      *Grade obtained
+      <v-text-field
+      label="Provide the final grade achieved"
+      :rules="[required]"
+      v-model="certificate.grade"
+      required>
+      </v-text-field>
+   </v-stepper-step>
+   <v-stepper-content step="3">
+      <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
+      <v-btn flat @click="e6 = 2">Cancel</v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="4">
+      *Validity
+      <v-text-field
+      label="Expiry date"
+      :rules="[required]"
+      v-model="certificate.validity"
+      required>
+      </v-text-field>
+    </v-stepper-step>
+    <v-stepper-content step="4">
+      <v-btn color="primary" @click="e6 = 1">Upload</v-btn>
+      <v-btn flat @click="e6 = 3">Cancel</v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="5">
+      *Comments
+      <v-text-field
+      label="Additional comments"
+      multi-line
+      v-model="certificate.comment">
+      </v-text-field>
+    </v-stepper-step>
+    <v-stepper-content step="5">
+      <v-btn color="primary" @click="e6 = 1">Upload</v-btn>
+      <v-btn flat @click="e6 = 4">Cancel</v-btn>
+    </v-stepper-content>
+  </v-stepper>
               </panel>
           </v-flex>
         </v-layout>
@@ -26,6 +91,12 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    }
+  },
+  data () {
+    return {
+
+      e6: 1
     }
   }
 }
