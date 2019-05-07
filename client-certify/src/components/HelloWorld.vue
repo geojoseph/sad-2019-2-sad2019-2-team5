25060eb5ce8bd886570b1663532cc1b1eb07e05b
Geo <geojoseph.mec@gmail.com>
2019-05-07 23:52:39 +0200

    Home Page Update

--- client-certify/src/components/HelloWorld.vue
+++ client-certify/src/components/HelloWorld.vue
@@ -1,113 +1,119 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="mainpage">
+    <div class="container">
+      <div class="layout align-center justify-center wrap">
+        <div class="v-responsive v-image shrink mr-5" style="height: 256px; width: 256px;">
+          <div class="v-responsive__sizer" style="padding-bottom: 100%;"></div>
+          <!-- make sure it is outside a tag
+          <div
+            class="v-image__image v-image__image--contain"
+            style="background-image: url(&quot;C:\Users\Admin\Pictures\Cropped.png&quot;); background-position: center center;"
+          ></div>
+           -->
+          <div id="app1">
+            <img src="../assets/homepage.jpg">
+          </div>
+          <div class="v-responsive__content"></div>
+        </div>
+        <div class="text-xs-center text-lg-left">
+          <h1 class="font-weight-light mb-4 display-3">"Welcome to Certify App"</h1>
+        </div>
+      </div>
+    </div>
+    <div class="v-responsive text-xs-center pb-4">
+      <div class="v-responsive__content">
+        <div class="container grid-list-xl">
+          <div class="layout justify-center wrap">
+            <div class="flex d-flex shrink">
+              <div
+                class="elevation-12 hide-overflow text-xs-center mx-auto v-card v-sheet theme--light"
+                style="max-width:350px;"
+              >
+                <div
+                  role="img"
+                  aria-label="Vibrant Community"
+                  class="v-responsive v-image"
+                  style="width:100%;"
+                >
+                  <div class="v-responsive__sizer" style="padding-bottom:38.46153846153846%;"></div>
+                  <div
+                    class="v-image__image v-image__image--cover"
+                    style="background-image: url(&quot;https://cdn.vuetifyjs.com/images/home/feature3.png&quot;); background-position: center center;"
+                  ></div>
+                  <div class="v-responsive__content"></div>
+                </div>
+                <div class="v-card__text">
+                  <h3 class="subheading font-weight-bold mb-2">Unified Platform</h3>
+                  <p
+                    class="mb-2"
+                  >No more confusion!, A single platform for universities to distribute their certificates..</p>
+                </div>
+              </div>
+            </div>
+            <div class="flex d-flex shrink">
+              <div
+                class="elevation-12 hide-overflow text-xs-center mx-auto v-card v-sheet theme--light"
+                style="max-width:350px;"
+              >
+                <div
+                  role="img"
+                  aria-label="Semantic Material Components"
+                  class="v-responsive v-image"
+                  style="width:100%;"
+                >
+                  <div class="v-responsive__sizer" style="padding-bottom:38.46153846153846%;"></div>
+                  <div
+                    class="v-image__image v-image__image--cover"
+                    style="background-image: url(&quot;https://cdn.vuetifyjs.com/images/home/feature2.png&quot;); background-position: center center;"
+                  ></div>
+                  <div class="v-responsive__content"></div>
+                </div>
+                <div class="v-card__text">
+                  <h3 class="subheading font-weight-bold mb-2">Distributed power</h3>
+                  <p
+                    class="mb-2"
+                  >Say Good Bye to server Crashes!, By utilizing our platform you can bring the distributed computing power..</p>
+                </div>
+              </div>
+            </div>
+            <div class="flex d-flex shrink">
+              <div
+                class="elevation-12 hide-overflow text-xs-center mx-auto v-card v-sheet theme--light"
+                style="max-width:350px;"
+              >
+                <div
+                  role="img"
+                  aria-label="Ready-Made Project Scaffolding"
+                  class="v-responsive v-image"
+                  style="width:100%;"
+                >
+                  <div class="v-responsive__sizer" style="padding-bottom:38.46153846153846%;"></div>
+                  <div
+                    class="v-image__image v-image__image--cover"
+                    style="background-image: url(&quot;https://cdn.vuetifyjs.com/images/home/feature1.png&quot;); background-position: center center;"
+                  ></div>
+                  <div class="v-responsive__content"></div>
+                </div>
+                <div class="v-card__text">
+                  <h3 class="subheading font-weight-bold mb-2">Easy to validate</h3>
+                  <p
+                    class="mb-2"
+                  >Don't worry about the genuinity of certificates, A single stop destination for verifying certificates..</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Certify'
-    }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
