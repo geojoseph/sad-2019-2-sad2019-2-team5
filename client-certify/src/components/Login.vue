9723375cb2d531f8ad59121caac76c82d872970d
Sreehari Mullapulli <sreehari.sm@gmail.com>
2019-05-08 02:28:55 +0200

    login and register template update

--- client-certify/src/components/Login.vue
+++ client-certify/src/components/Login.vue
@@ -3,7 +3,7 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
+          <v-flex xs8>
             <panel title="Login">
               <v-form>
                 <v-text-field name="email" label="Email" v-model="email" type="email"></v-text-field>
